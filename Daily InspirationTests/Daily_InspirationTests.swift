b6ba7f397d3e0a6ae01fbd39cbfabe9bcfbb3a79
ldur <lassedur@gmail.com>
2025-07-29 13:50:15 +0200

    Initial Commit

---
+++ Daily InspirationTests/Daily_InspirationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_InspirationTests.swift
+//  Daily InspirationTests
+//
+//  Created by Lasse Durucz on 29/07/2025.
+//
+
+import Testing
+@testable import Daily_Inspiration
+
+struct Daily_InspirationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
